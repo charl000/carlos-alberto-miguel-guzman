d8e40caf972e22dd6fee8fac18c7832a05fe9a3a
charl000 <90639786+charl000@users.noreply.github.com>
2021-09-13 11:46:05 -0500

    Add files via upload

---
+++ codigo_practica_1.ino
@@ -0,0 +1,14 @@
+float v= 0.0;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(A0, INPUT);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  v = analogRead(A0);
+  delay(10000);
+
+}
